77ae068dfdbe51d0f74d31ba5b9b3003ec8c1c05
Aayush Naik <aayushnai@gmail.com>
2024-11-30 00:13:31 +0530

    fix iam role for task def

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,29 @@ resource "aws_security_group" "ecs_tasks_sg" {
   }
 }
 
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 # Elasticsearch Task Definition
 resource "aws_ecs_task_definition" "elasticsearch_task" {
   family                   = "elasticsearch"
@@ -68,6 +91,7 @@ resource "aws_ecs_task_definition" "elasticsearch_task" {
   network_mode            = "awsvpc"
   cpu                     = 1024
   memory                  = 2048
+  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -117,6 +141,7 @@ resource "aws_ecs_task_definition" "tika_task" {
   network_mode            = "awsvpc"
   cpu                     = 512
   memory                  = 1024
+  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
