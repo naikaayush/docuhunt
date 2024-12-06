ecb3084d22866c94302690a84209655dfb7aaeed
Aayush Naik <aayushnai@gmail.com>
2024-12-06 10:01:12 +0100

    rm duplicate http listener

--- terraform/main.tf
+++ terraform/main.tf
@@ -304,18 +304,23 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.docuhunt_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+resource "aws_lb_listener_rule" "http_redirect" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 1
 
-  default_action {
+  action {
     type = "redirect"
     redirect {
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
       host        = "api.docuhunt.me"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
     }
   }
 }
